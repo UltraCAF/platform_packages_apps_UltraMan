ee235ab276889b9ed971663be70b6de84e5d5f95
Ahmed Hady <ahmedhady6@gmail.com>
2017-06-21 16:13:01 +0200

    (1/2)Add about us

--- java/com/ultra/manager/about/AboutActivity.java
+++ java/com/ultra/manager/about/AboutActivity.java
@@ -1,16 +1,16 @@
-package com.ultra.manager.lights;
+package com.ultra.manager.about;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 import com.ultra.manager.R;
 
-public class LedActivity extends PreferenceActivity {
+public class AboutActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new LedFragment()).commit();
+                .replace(android.R.id.content, new AboutFragment()).commit();
     }
 }
