f89c9dc5f2bafc4800858cb76a95b1a5eb0b1f12
Ahmed Hady <ahmedhady6@gmail.com>
2017-06-21 17:17:03 +0200

    (1/2) Moving CAF Version to UltraMan

--- java/com/ultra/manager/about/AboutFragment.java
+++ java/com/ultra/manager/about/AboutFragment.java
@@ -6,12 +6,16 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 
 import com.ultra.manager.R;
 import com.ultra.manager.utils.SettingsPreferenceFragment;
 
 public class AboutFragment extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String KEY_CAF_VERSION = "caf_version";
 
     private static final String TAG = "AboutPerfs";
 
@@ -25,6 +29,8 @@ public class AboutFragment extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.perf_about);
+
+        setValueSummary(KEY_CAF_VERSION, "ro.caf.version");
     }
 
     @Override
@@ -37,5 +43,15 @@ public class AboutFragment extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
+    }
+
+    private void setValueSummary(String preference, String property) {
+        try {
+            findPreference(preference).setSummary(
+                    SystemProperties.get(property,
+                            getResources().getString(R.string.device_info_default)));
+        } catch (RuntimeException e) {
+            // No recovery
+        }
     }
 }
